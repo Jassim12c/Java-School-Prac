5a7d4f552aca5ac9caf065bb9b84bb2ca2d67337
Jassim12c <duod2003row@gmail.com>
2022-02-22 13:57:18 +0300

    new questions done

--- lab1/test1.java
+++ lab1/test1.java
@@ -1,9 +1,6 @@
 package lab1;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class test1 {
     public static void main(String[] args) {
@@ -131,19 +128,29 @@ public class test1 {
 
         // A
         int[][] givenArray = {{15, 9, 10, 41}, {22, 13, 63, 50}, {85, 27, 3, 15}, {19, 75, 11, 5}};
+        for(int i=0; i<=givenArray.length-1; i++){
+            for(int j=0; j<=givenArray.length-1; j++){
+                System.out.println(givenArray[i][j]);
+            }
+        }
+
         // B
         int total = 0;
         int compareTotal = 0;
+        int ElemNum = 0;
         for(int i=0; i <= givenArray.length-1; i++){
             for(int j=0; j <= givenArray[i].length-1; j++){
                 compareTotal += givenArray[i][j];
-                if(total < compareTotal){
-                    total = compareTotal;
-                    compareTotal = 0;
-                }
+            }
+            if(total < compareTotal){
+                total = compareTotal;
+                compareTotal = 0;
+                ElemNum = i;
+            }else{
+                compareTotal = 0;
             }
         }
-        System.out.println("total of list: " + total);
+        System.out.println("total of list: " + total + " The list is in the " + ElemNum + " row");
 
 
         // C
@@ -154,6 +161,7 @@ public class test1 {
                 if(givenArray[i][j] > 10 && givenArray[i][j] < 50){
                     count++;
                 }
+                // d
                 if(i%2==0 && j%2==0){
                     sumOfEven += givenArray[i][j];
                 }
@@ -162,9 +170,49 @@ public class test1 {
         System.out.println("Between 10 and 50 " + count);
         System.out.println("Sum of even index position numbers " + sumOfEven);
 
+        // e
+        int[][] tranArr = new int[givenArray.length][givenArray[0].length];
+        for(int i=0; i<=givenArray.length-1; i++){
+            for(int j=0; j<=givenArray[i].length-1; j++){
+                tranArr[j][i] = givenArray[i][j];
+            }
+        }
 
-        for(int i=givenArray.length-1; i>=0; i--){
-            ;
+        System.out.println(Arrays.deepToString(tranArr));
+
+        // Q4
+        int[][] medianNum = {{21, 14, 13, 12, 15}, {4, 25, 23, 22, 9}, {4, 7, 8, 98, 24}};
+        int numberOfElements = 0;
+        for(int i=0; i<=medianNum.length-1; i++){
+            for(int j=0; j<=medianNum[i].length-1; j++){
+                numberOfElements++;
+            }
+        }
+        System.out.println(numberOfElements);
+        int[] newArray = new int[numberOfElements];
+        int counter = 0;
+        for(int i=0; i<=medianNum.length-1; i++){
+            for(int j=0; j<=medianNum[i].length-1; j++){
+                newArray[counter] = medianNum[i][j];
+                counter++;
+            }
+        }
+        Arrays.sort(newArray);
+
+        System.out.println(Arrays.toString(newArray));
+
+        if(newArray.length % 2 > 0){
+            int n = (newArray.length-1)/2;
+            System.out.println(Arrays.toString(newArray));
+            System.out.println(newArray[n]);
+        }else{
+            int n = (newArray.length/2);
+            int secondN = n-1;
+            if(newArray[n] == newArray[secondN]){
+                System.out.println(newArray[n]);
+            }else{
+                System.out.println( (double) (newArray[n] + newArray[secondN])/2);
+            }
         }
 
     }
